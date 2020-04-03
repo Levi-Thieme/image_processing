67d6802ec13254a6ab31069386ddadbf8292e1b4
Levi-Thieme <thielt01@pfw.edu>
2020-04-03 17:42:23 -0400

    Added folders for original, compressed, and permuted images.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from transform import *
 import numpy as np
 import keygen
 import chaos
-
-image = getImageData("images/lena.gif").astype(float)
+name = "camera"
+image = getImageData("images/" + name + ".gif").astype(float)
 
 transformed_image = blockwiseDCT(image)
 
@@ -18,6 +18,8 @@ print("Keeping only %f%% of the DCT coefficients" %(percent_nonzeros*100.0))
 dct_image = blockwiseIDCT(image, threshold)
 dct_image = normalize(0.0, 1.0, dct_image)
 
+saveImage(dct_image, "./compressed_images/" + name + ".jpg")
+
 states_and_key = keygen.create_key_and_init_states(dct_image)
 init_x = states_and_key[0]
 init_y = states_and_key[1]
@@ -28,6 +30,6 @@ x0 = init_x[0]
 y0 = init_y[0]
 [row_permutations, column_permutations] = chaos.log_map_sequences(1.13, x0, y0, imsize[0])
 permuted_image = swap_rows(dct_image, row_permutations)
-saveImage(permuted_image, "./permuted_image.jpg")
+saveImage(permuted_image, "./permuted_images/row_permuted_" + name + ".jpg")
 permuted_image = swap_columns(permuted_image, column_permutations)
-saveImage(permuted_image, "./final_permuted_image.jpg")
+saveImage(permuted_image, "./permuted_images/rowcolumn_permuted" + name + ".jpg")
