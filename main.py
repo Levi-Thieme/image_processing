a8a5cbc276316d0bf14d3678721c4222511ceab6
Levi-Thieme <thielt01@pfw.edu>
2020-05-04 21:34:54 -0400

    Updated main.py to use arguments for filepath and name of image.

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from transform import *
 import numpy as np
 import keygen
 import chaos
-name = "camera"
-image = getImageData("images/" + name + ".gif").astype(float)
+import sys
+
+if (len(sys.argv) < 3):
+	print("Please enter a filepath and image name of the image to process.")
+	exit()
+
+filepath = sys.argv[1]
+name = sys.argv[2]
+image = getImageData(filepath).astype(float)
 
 transformed_image = blockwiseDCT(image)
 
@@ -18,7 +25,7 @@ print("Keeping only %f%% of the DCT coefficients" %(percent_nonzeros*100.0))
 dct_image = blockwiseIDCT(image, threshold)
 dct_image = normalize(0.0, 1.0, dct_image)
 
-saveImage(dct_image, "./compressed_images/" + name + ".jpg")
+saveImage(dct_image, "compressed" + name + ".jpg")
 
 states_and_key = keygen.create_key_and_init_states(dct_image)
 init_x = states_and_key[0]
@@ -30,6 +37,6 @@ x0 = init_x[0]
 y0 = init_y[0]
 [row_permutations, column_permutations] = chaos.log_map_sequences(1.13, x0, y0, imsize[0])
 permuted_image = swap_rows(dct_image, row_permutations)
-saveImage(permuted_image, "./permuted_images/row_permuted_" + name + ".jpg")
+saveImage(permuted_image, "row_permuted_" + name + ".jpg")
 permuted_image = swap_columns(permuted_image, column_permutations)
-saveImage(permuted_image, "./permuted_images/rowcolumn_permuted" + name + ".jpg")
+saveImage(permuted_image, "rowcolumn_permuted" + name + ".jpg")
